2d5a6c7af56cf9cccbe6f9686cc7c2506564cb93
andersonstorqui <andersongmail.com>
2023-05-14 13:30:10 -0300

    fit; criando estrutura

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>PortFólio</title>
+</head>
+<body>
+    <header class="header">
+        <div class="nameTitle">
+            <div class="circle"></div>
+            <div class="title">Anderson da Silva</div>
+            <div class="work">Desenvolvedor Web</div>
+        </div>
+        <div class="navBar">
+            <a href="" class="navBarItem">Currículo</a>
+            <a href="" class="navBarItem">Projetos</a>
+            <a href="" class="navBarItem">Contato</a>
+        </div>
+    </header>
+    <section>
+        <article class="article">
+        <figure class="img">
+            <img src="imagem.jpg" alt="Descrição da imagem" class="imagem">
+            <figcaption>Dev FullStack</figcaption>
+            </figure>
+            <div class="div_title">
+                <h1 class="title_hello">Olá!</h1>
+            </div>
+            <div class="div_paragraph">
+                <p class="paragraph_hello">Sou um parágrafo. Clique aqui para adicionar seu texto. Sou um ótimo lugar para você contar a sua história e compartilhar um pouco mais sobre você.</p>
+            </div>
+        <article class="content_menus">
+            <div class="content_menus_div">
+                <div class="class_content_curriculo">
+                    <a href="www.google.com" class="ancora">
+                        <p class="p_itens">Currículo</p>    
+                    </a>
+                </div>
+                    <div class="class_content_projetos">
+                        <a href="www.google.com" class="ancora">
+                            <p class="p_itens">Projetos</p>
+                        </a>
+                    </div>
+                    <div class="class_content_contato">
+                        <a href="www.google.com" class="ancora">
+                             <p class="p_itens">Contato</p>
+                        </a>
+                    </div>
+                </article>
+            </div>
+ </article>
+</section>
+</body>
+</html>
